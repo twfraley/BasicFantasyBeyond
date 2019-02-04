da761c1519df21ac199270aa1288c903e1c66cb0
twfraley <twfraley@gmail.com>
2019-02-04 14:46:37 -0500

    commit

---
+++ BasicFantasyBeyond/Models/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicFantasyBeyond.Models
+{
+    public 
+
+    public class Character
+    {
+        public string CharacterName { get; set; }
+        public string Race { get; set; }
+    }
+}
