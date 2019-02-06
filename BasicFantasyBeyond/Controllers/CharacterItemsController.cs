fd9c344f97429854dd9172d9a8c9851bab2839ed
twfraley <twfraley@gmail.com>
2019-02-06 11:46:10 -0500

    commit

---
+++ BasicFantasyBeyond/Controllers/CharacterItemsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicFantasyBeyond.Controllers
+{
+    public class CharacterItemsController : Controller
+    {
+        // GET: CharacterItems
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BasicFantasyBeyond/Controllers/EquipmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicFantasyBeyond.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        // GET: Equipment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
