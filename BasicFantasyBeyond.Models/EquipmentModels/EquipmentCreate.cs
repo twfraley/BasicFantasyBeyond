cd9c3f26e06f8dafa0605bbdfafc9205be5313bc
twfraley <twfraley@gmail.com>
2019-02-07 15:43:57 -0500

    commit

--- BasicFantasyBeyond.Models/EquipmentModels/EquipmentCreate.cs
+++ BasicFantasyBeyond.Models/EquipmentModels/EquipmentCreate.cs
@@ -1,33 +1,23 @@
-﻿using BasicFantasyBeyond.Models.EquipmentModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BasicFantasyBeyond.Models
+namespace BasicFantasyBeyond.Models.EquipmentModels
 {
+    public enum EquipmentType { Weapon, Armor, Shield, Gear }
+    public enum DamageType { Piercing, Bludgeoning, Slashing }
 
-    public class Equipment
+    public class EquipmentCreate
     {
-        [Key]
-        public int ItemID { get; set; }
-
         [Required]
         public string ItemName { get; set; }
-
-        [Required]
         public EquipmentType EquipmentType { get; set; }
-
-        public bool IsEquipped { get; set; }
-
         public string Damage { get; set; }
-
         public DamageType? DamageType { get; set; }
-
         public int? ArmorClassBonus { get; set; }
-
         public string ItemNotes { get; set; }
     }
 }
--- BasicFantasyBeyond.Services/EquipmentServices.cs
+++ BasicFantasyBeyond.Services/EquipmentServices.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BasicFantasyBeyond.Data;
+using BasicFantasyBeyond.Models;
+using BasicFantasyBeyond.Models.CharacterModels;
+using BasicFantasyBeyond.Models.EquipmentModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,109 @@ namespace BasicFantasyBeyond.Services
 {
     public class EquipmentServices
     {
+        private readonly Guid _userID;
+
+        public EquipmentServices(Guid userID)
+        {
+            userID = _userID;
+        }
+
+        public bool CreateEquipment(EquipmentCreate model)
+        {
+            var entity =
+                new Equipment()
+                {
+                    ItemName = model.ItemName,
+
+                };
+
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                ctx.Equipment.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<Equipment> GetEquipment()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx.
+                    Equipment
+                    .Select(
+                        e =>
+                        new Equipment
+                        {
+                            ItemID = e.ItemID,
+                            ItemName = e.ItemName,
+                            EquipmentType = e.EquipmentType,
+                            IsEquipped = e.IsEquipped,
+                            Damage = e.Damage,
+                            DamageType = e.DamageType,
+                            ArmorClassBonus = e.ArmorClassBonus,
+                            ItemNotes = e.ItemNotes
+                        });
+                return query.ToArray();
+            }
+        }
+
+        public Equipment GetEquipmentByID(int equipmentID)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Equipment
+                    .Single(e => e.ItemID == equipmentID);
+                return
+                    new Equipment
+                    {
+                        ItemID = entity.ItemID,
+                        ItemName = entity.ItemName,
+                        EquipmentType = entity.EquipmentType,
+                        IsEquipped = entity.IsEquipped,
+                        Damage = entity.Damage,
+                        DamageType = entity.DamageType,
+                        ArmorClassBonus = entity.ArmorClassBonus,
+                        ItemNotes = entity.ItemNotes
+                    };
+            }
+        }
+
+        public bool UpdateEquipment(Equipment model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Equipment
+                    .Single(e => e.ItemID == model.ItemID);
+
+                entity.ItemID = model.ItemID;
+                entity.ItemName = model.ItemName;
+                entity.EquipmentType = model.EquipmentType;
+                entity.IsEquipped = model.IsEquipped;
+                entity.Damage = model.Damage;
+                entity.DamageType = model.DamageType;
+                entity.ArmorClassBonus = model.ArmorClassBonus;
+                entity.ItemNotes = model.ItemNotes;
+                
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteEquipment(int equipmentID)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Equipment.Single(e => e.ItemID == equipmentID);
+
+                ctx.Equipment.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
     }
 }
