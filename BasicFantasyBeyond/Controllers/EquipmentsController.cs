6150a33811077af9b52f6b1ea2f75585bb51e0cf
twfraley <twfraley@gmail.com>
2019-02-07 15:59:29 -0500

    commitment to safety

---
+++ BasicFantasyBeyond/Controllers/EquipmentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BasicFantasyBeyond.Data;
+using BasicFantasyBeyond.Models;
+
+namespace BasicFantasyBeyond.Controllers
+{
+    public class EquipmentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Equipments
+        public ActionResult Index()
+        {
+            return View(db.Equipment.ToList());
+        }
+
+        // GET: Equipments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipment.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,ItemName,EquipmentType,IsEquipped,Damage,DamageType,ArmorClassBonus,ItemNotes")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipment.Add(equipment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipment.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,ItemName,EquipmentType,IsEquipped,Damage,DamageType,ArmorClassBonus,ItemNotes")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipment.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipment equipment = db.Equipment.Find(id);
+            db.Equipment.Remove(equipment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
