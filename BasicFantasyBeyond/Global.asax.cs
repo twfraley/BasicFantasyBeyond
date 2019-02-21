bdc5029c5dca3c72ae2065dd4baa74fee75f7126
twfraley <twfraley@gmail.com>
2019-02-21 12:14:44 -0500

    commit

--- BasicFantasyBeyond/Global.asax.cs
+++ BasicFantasyBeyond/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BasicFantasyBeyond.Models.CharacterModels;
+using MvcEnumFlags;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +18,7 @@ namespace BasicFantasyBeyond
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.Add(typeof(CharacterAbilities), new EnumFlagsModelBinder());
         }
     }
 }
