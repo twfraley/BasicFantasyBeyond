a107e626157f1873ae6bfdb5d4dd53b510fc8eb9
twfraley <twfraley@gmail.com>
2019-02-25 09:20:51 -0500

    Added Unit Tests

---
+++ BasicFantasyBeyond.Tests/CharacterItemServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicFantasyBeyond.Tests
+{
+    [TestClass]
+    public class CharacterItemServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BasicFantasyBeyond.Tests/CharacterServicesTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicFantasyBeyond.Tests
+{
+    [TestClass]
+    public class CharacterServicesTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+
+    }
+}
